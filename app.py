838a42e7a2e5af988121e3cc8f5152fb1e96f283
Tafari Lewis <tafari.lewis@mymona.uwi.edu>
2019-02-06 09:48:14 +0000

    make it better

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
  return 'My home page'
+ 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+@app.route('/features')
+def features():
+    render_template('features.html')
 
 
 @app.errorhandler(404)
